00a83f3acef39a83ddca3720dfa2331ccc3cfaf5
Jorge Rodriguez <Jorge@Jorge-PC>
2013-08-08 00:43:30 -0500

    Pagina Index de OnlinePoll

---
+++ OnlinePoll/index.php
@@ -0,0 +1,26 @@
+<?php
+    require('conexion.php');
+    $sql = "SELECT * FROM encuestas ORDER BY id DESC";
+    $req = mysql_query($sql);
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+    <div class="wrap">
+        <h1>Consulta</h1>
+        <ul class="votacion index">
+        <?php
+            while($result = mysql_fetch_object($req)){
+                echo '<li><a href="encuesta.php?id='.$result->id.'">'.$result->titulo.'</a></li>';
+            }
+        ?>
+        </ul>
+       
+    </div>
+</body>
+</html>
