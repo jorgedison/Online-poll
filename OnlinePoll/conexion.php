43678bd6d9e60953336a4c7d1cb74b7262600c39
abdelyazid <jorgeedison1@gmail.com>
2013-09-07 11:19:32 -0500

    Update conexion.php

--- OnlinePoll/conexion.php
+++ OnlinePoll/conexion.php
@@ -2,7 +2,9 @@
 define('DB_SERVER','localhost');
 define('DB_NAME','encuesta');
 define('DB_USER','root');
-define('DB_PASS','jorge');
+define('DB_PASS','JUkjhkhhu');
+#define('DB_PASS','jorge');
+
  
 $conex = mysql_connect (DB_SERVER,DB_USER,DB_PASS);
 mysql_select_db(DB_NAME,$conex);
